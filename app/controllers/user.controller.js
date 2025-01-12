b39d5f517f1716ec1db0ef8aab0d3f2929f17e96
pankti13 <pankti221365@gmail.com>
2025-01-12 17:01:39 +0530

    Add role-specific functionalities

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -38,4 +38,81 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { register, login };
+const deleteUser = async (req, res) => {
+    try {
+        const userId = req.query.userId;
+        if (!userId) {
+            return res.status(400).json({ error: "User ID is required" });
+        }
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        await User.findByIdAndDelete(userId);
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete user" });
+    }
+};
+
+const getUserProfile = async (req, res) => {
+    try {
+        let userId;
+        if (req.user.role === "admin" && req.query.userId) {
+            userId = req.query.userId;
+        } else {
+            userId = req.user.id;
+        }
+        // If the logged-in user is an admin, they can get any user's profile.
+        if (req.user.role === "admin") {
+            const user = await User.findById(userId);
+            return res.status(200).json(user);
+        }
+        // If the logged-in user is a regular user, they can only access their own profile
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch user profile" });
+    }
+};
+
+const updateUserProfile = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userId = req.user.id;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        let updated = 0;
+        // Update the email if provided
+        if (email) {
+            const existingUser = await User.findOne({ email });
+            if (existingUser && existingUser.id !== userId) {
+                return res.status(400).json({ error: "Email already in use" });
+            }
+            user.email = email;
+            updated = 1;
+        }
+        // Update the password if provided
+        console.log(password);
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            updated = 1; 
+        }
+        await user.save();
+        if (!updated) {
+            return res.status(400).json({ message: "No data provided to update" });
+        }
+        return res.status(200).json({ message: "User profile updated successfully" });
+        } catch (error) {
+        res.status(500).json({ error: "Failed to update user profile" });
+    }
+};
+
+module.exports = { register, login, deleteUser, getUserProfile, updateUserProfile };
--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { register, login} = require("../controllers/user.controller");
+const { register, login, deleteUser, getUserProfile, updateUserProfile} = require("../controllers/user.controller");
 const { verifyToken, checkRole } = require("../middlewares/authorizeJwt");
 
 router.post("/register", register);
 router.post("/login", login);
 
 // Protected routes (JWT and role-based authorization)
-router.get("/admin-dashboard", verifyToken, checkRole(["admin"]), (req, res) => {
-    res.send("Welcome to the admin dashboard");
-});
-
-router.get("/user-dashboard", verifyToken, checkRole(["user", "admin"]), (req, res) => {
-    res.send("Welcome to the user dashboard");
-});
+router.delete("/admin-dashboard", verifyToken, checkRole(["admin"]), deleteUser);
+router.get("/user-dashboard", verifyToken, checkRole(["user", "admin"]), getUserProfile);
+router.put("/user-dashboard", verifyToken, checkRole(["user"]), updateUserProfile);
 
 module.exports = router;
